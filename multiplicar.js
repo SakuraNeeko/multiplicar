86421c9a97b64ce1f38b5add0252ecbb94b5d0e5
SakuraNeeko <bryan_zac22@hotmail.com>
2026-04-28 10:42:30 -0500

    feat: tabla dinamica

--- multiplicar.js
+++ multiplicar.js
@@ -1,4 +1,10 @@
 function generarTablas() {
     let contenedor = document.getElementById("contenedor");
+    let contenido = "";
+    
+    for (let i = 1; i <= 10; i++) {
+        contenido += `<div class="fila">3 x ${i} = ${3 * i}</div>`;
+    }
+    
     contenedor.innerHTML = "<h1>PROBANDO</h1>";
 }
