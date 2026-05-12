15623acffed6e9b619076d99927545d432d2b1f6
SakuraNeeko <bryan_zac22@hotmail.com>
2026-05-12 15:47:10 -0500

    feat: styles and js

--- multiplicar.js
+++ multiplicar.js
@@ -1,10 +1,18 @@
 function generarTablas() {
+    let inputValor = document.getElementById("numero").value;
     let contenedor = document.getElementById("contenedor");
+    
+    if (inputValor === "") {
+        contenedor.innerHTML = '<div class="mensaje-inicial">¡Por favor ingresa un número primero! (╥﹏╥)</div>';
+        return; 
+    }
+
+    let num = parseInt(inputValor);
     let contenido = "";
     
     for (let i = 1; i <= 10; i++) {
-        contenido += `<div class="fila">5 x ${i} = ${5 * i}</div>`;
+        contenido += `<div class="fila">${num} x ${i} = ${num * i}</div>`;
     }
     
-    contenedor.innerHTML = "<h1>PROBANDO</h1>";
+    contenedor.innerHTML = contenido;
 }
